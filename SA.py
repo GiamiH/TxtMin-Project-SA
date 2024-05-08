aac8d059e94017ce6c9bd41992bdc0bd35394cd9
Maria <marmarbar003@gmail.com>
2024-05-08 21:16:53 +0200

    Filter amazon customer rev

--- SA.py
+++ SA.py
@@ -7,15 +7,31 @@ Created on Wed May  8 20:00:52 2024
 
 import json
 
-with open('Iphone11-R.json', 'r') as file:
-    data = json.load(file)
 
-n = 0
-for r in data:
+# open file
+with open('Iphone11-R.json', 'r') as file:
+    og = json.load(file)
+
+# deletes useless col
+for r in og:
     del r["product_company"]
     del r["product"]
     del r["review_country"]
     del r["total_comments"]
     del r["reviewed_at"]
     del r["url"]
+    
+# simplifies start to a single number (1 - 5)
+for r in og:
+    s = r["review_rating"]
+    r["review_rating"] = s[0]
+
+# Removes any review without a name
+data = []
+for r in og:
+    if r["profile_name"] != "Amazon customer" and r["profile_name"] != "Amazon Customer":
+        data.append(r)
+        
+        
+    
     
