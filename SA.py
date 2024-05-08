bc8c5b39545a87bbf00efaa9b25e6d518350cc19
Maria <marmarbar003@gmail.com>
2024-05-08 20:32:34 +0200

    Initial cleanse of col

--- SA.py
+++ SA.py
@@ -5,3 +5,17 @@ Created on Wed May  8 20:00:52 2024
 @author: marma
 """
 
+import json
+
+with open('Iphone11-R.json', 'r') as file:
+    data = json.load(file)
+
+n = 0
+for r in data:
+    del r["product_company"]
+    del r["product"]
+    del r["review_country"]
+    del r["total_comments"]
+    del r["reviewed_at"]
+    del r["url"]
+    
