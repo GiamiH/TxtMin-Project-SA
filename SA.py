e4ee275a784556a16f442587ad79e3c8c7dd6ef2
Maria <marmarbar003@gmail.com>
2024-05-14 13:45:29 +0200

    Skeleton for identifying gender via a prefix (ex. Mr and Ms

--- SA.py
+++ SA.py
@@ -7,7 +7,6 @@ Created on Wed May  8 20:00:52 2024
 
 import json
 
-
 # open file
 with open('Iphone11-R.json', 'r') as file:
     og = json.load(file)
@@ -33,8 +32,7 @@ data = []
 for r in og:
     s = r["profile_name"]
     if (s != "Amazon customer" and
-        s != "Amazon Customer" and not s[0].isdigit() and len(s) > 2
-        and len(s) !=1 ):
+        s != "Amazon Customer" and not s[0].isdigit() and len(s) > 2):
         data.append(r)
 
 # Helpful Count is an int
@@ -50,12 +48,45 @@ for r in data:
         r["helpful_count"] = int(n)
         
         s = r["helpful_count"]
-
-
         
- 
+# Take first names (Clear out Mr./Ms. and Dr.)
+for r in data:
+    prof_name = r["profile_name"]
+    split_name = list(prof_name)
+    c = 0
+    for i in split_name:
+        if i.isalpha():
+            c +=1
+        else:
+            break
+    
+# Testing with strings to identify namesvia prefix
+    
+split_4= list("Dr.dasdf")
+print(split_4)   
 
-        
-   
-    
-    
+c = 1
+pre = ""
+for i in split_4:
+        if i.isalpha():
+            c +=1
+            pre+=i
+        elif c==2:
+            for indx in range(0,c):
+                del split_4[0]
+        elif pre == "Mr":
+                print("Male")
+        elif pre == "Dr":
+            for indx in range(0,c):
+                del split_4[0]
+            print("delete :(")
+        elif(pre == "Ms"):
+                print("Female")
+        elif(pre == "Mrs"):
+                print("Female")
+        else:
+            print("Its a string of 3 char")
+            
+                
+
+print(split_4)   
