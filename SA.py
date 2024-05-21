0293838eed83c260131ad17a98cd000becc0a4b2
Maria <marmarbar003@gmail.com>
2024-05-21 23:29:46 +0200

    applying gender to Mr/Ms

--- SA.py
+++ SA.py
@@ -32,7 +32,7 @@ for r in og:
 data = []
 for r in og:
     s = r["profile_name"]
-    if (s != "Amazon customer" and s!= "" and
+    if (s != "Amazon customer" and s!= "" and s!="amazonUser" and
             s != "Amazon Customer" and not s[0].isdigit() and len(s) > 2):
         data.append(r)
 
@@ -58,92 +58,101 @@ p1 = r'\b[a-zA-Z]\.\s?'
 p2 = r'\b[a-zA-Z]\b'
 # Many spaces
 p3 = r'\s+'
+# Mr -> male
+mr = r'\bMr\.\b'
+# Ms -> female
+ms = r'\bMs\.\b'
+# Mrs -> female
+mrs = r'\bMrs\.\b'
+# Dr -> remove doctor & apply corpus
+dr = r'\bDr\.\b'
+
+
+# classifies gender with ms/mr
 for r in data:
     prof_name = r["profile_name"]
-    print(prof_name)
-    n1 = re.sub(p1, '', prof_name)
-    n2 = re.sub(p2, '', n1)
-    n3 = re.sub(p3, ' ', n2).strip()
-    r["profile_name"] = n3
     
-file_names = "pr_ames.txt"
+    if re.search(mr,prof_name):
+        gender = "male"
+    elif re.search(ms, prof_name) or re.search(mrs, prof_name):
+        gender = "female"
+    else:
+        n = re.sub(dr, '', prof_name)
+        r["profile_name"] = n
+    
+        n1 = re.sub(p1, '', n)
+        n2 = re.sub(p2, '', n1)
+        n3 = re.sub(p3, ' ', n2).strip()
+
+
+
+#First Names
+for l in data:
+    full_n = l["profile_name"]
+    w = full_n.split()
+    name = w[0]
+    l["profile_name"] = name
+
+
+
+
+# Created this file in order to check that all the filtering is done correctly    
+file_names = "names.txt"
 
 with open(file_names, 'w', encoding = 'utf-8') as f:
     for r in data:
         p = r["profile_name"]
         f.write(p + "\n")
-
-for l in data:
-    full_n = l["profile_name"]
-    if full_n == "":
-        
-    w = full_n.split()
-    name = w[0]
-    l["profile_name"] = name
-    
 ##############################################################################
 # Differentiate Dr. Mr. Ms. Mrs. w/regexp
 # After lunch apply it to names above
-
-t = "Babu.Abraham"
-mr = r'\bMr\.\b'
-ms = r'\bMs\.\b'
-mrs = r'\bMrs\.\b'
-n1 = re.sub(p1, '', t)
-n2 = re.sub(p2, '', n1)
-n3 = re.sub(p3, ' ', n2).strip()
-if re.search(mr, t):
-    t = "male"
-if re.search(ms, t):
-    t = "female"
-if re.search(mrs, t):
-    t = "female"
 
 # Replace any periods with a space
 per_space = r'\.'
 res = re.sub(per_space, ' ', n3 )
 print(res)
 
-#Doctor
-dr = r'\bDr\.\b'
+
 
 ##############################################################################
 
+# Sample dataset
 
-"""
-# Testing with strings to identify names via 
-split_4= list("sam a.asdam.")
-print(split_4)   
 
-c = 0
-pre = ""
-p=0
 
-for i in split_4:
-        if i == " ":
-            c=0
-            pre=""
-        elif i.isalpha():
-            c +=1
-            pre+=i
-        elif i == ".":
-            p +=1
-        elif c==2:
-            for indx in range(0,c+p):
-                del split_4[0]
-        elif pre == "Mr":
-                print("Male")
-        elif pre == "Dr":
-            for indx in range(0,c):
-                del split_4[0]
-            print("delete :(")
-        elif(pre == "Ms"):
-                print("Female")
-        elif(pre == "Mrs"):
-                print("Female")
-        else:
-            print("Its a string of 3 char")
-         
+data = [
+    {"profile_name": "Mr.asd"},
+    {"profile_name": "Mrs.fda"},
+    {"profile_name": "Ms.fad"},
+    {"profile_name": "fda"},
+    {"profile_name": "Ms.hither"},
+    {"profile_name": "Mr.lol"}
+]
 
-"""
+# Process each profile name and add a new column for gender
+for r in data:
+    prof_name = r["profile_name"]
+    
+    
+    # Determine the gender based on the cleaned profile name
+    if re.search(mr, prof_name):
+        gender = "male"
+    elif re.search(ms, prof_name) or re.search(mrs, prof_name):
+        gender = "female"
+    else:
+        # Apply the substitutions
+        n1 = re.sub(p1, '', prof_name)
+        n2 = re.sub(p2, '', n1)
+        n3 = re.sub(p3, ' ', n2).strip()
+        
+        gender = "none"
+        r["profile_name"] = n3
+    
+    # Add the gender to the dictionary
+    r["gender"] = gender
+    
+
+# Print the updated dataset
+for r in data:
+    print(r)
 
