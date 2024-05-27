6dccedc35f9736cbbf3e210f1caabb8f98df6f42
hannahhommel <159558546+hannahhommel@users.noreply.github.com>
2024-05-27 20:19:50 +0200

    Added Code for Plotting Word Ratio

---
+++ Plot_Word_Ratio.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Male Reviews 
+x = np.array(["Unique Words", "Repeated Words"])
+y = np.array([1926, 12067])
+
+font1 = {'family':'serif','color':'black','size':20}
+
+plt.title("Male Repition Ratio = 6.27", fontdict = font1)
+plt.bar(x,y)
+plt.show()
+
+plt.savefig('Male_Word_Ratio.png')
+
+# Female Reviews 
+x2 = np.array(["Unique Words", "Repeated Words"])
+y2 = np.array([1475, 7407])
+
+plt.title("Female Repition Ratio = 5.02", fontdict = font1)
+plt.bar(x2, y2)
+plt.show()
+
+plt.savefig('Female_Word_Ratio.png')
