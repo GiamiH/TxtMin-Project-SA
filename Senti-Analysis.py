af727d337f31740545cb7ca983dfb69120e3a809
Giami <huynhgiami@gmail.com>
2024-05-27 13:50:44 +0200

    gender-classified counted

--- Senti-Analysis.py
+++ Senti-Analysis.py
@@ -1,17 +1,10 @@
 import pandas as pd
 from textblob import TextBlob
-from transformers import pipeline
-import json
-from collections import Counter
 
-with open('Iphone11-R.json', 'r') as file:
-    data = json.load(file)
-
-# Convert data to DataFrame
-reviews_df = pd.DataFrame(data)
-
-# Remove unnecessary columns
-reviews_df = reviews_df[['review_text', 'review_rating']]
+file_path = "/Users/giamihuynh/Downloads/TxtMin-Project-SA-1/Processed_Data/Final_Gendered_2.csv"
+reviews_df = pd.read_csv(file_path)
+reviews_df = reviews_df[reviews_df['gender'] != 'unknown']
+reviews_df['review_text'] = reviews_df['review_text'].fillna('').astype(str)
 
 # Sentiment Analysis with TextBlob
 def get_sentiment(text):
@@ -22,5 +15,5 @@ reviews_df['sentiment'] = reviews_df['review_text'].apply(get_sentiment)
 
 # Classify the sentiment
 reviews_df['sentiment_label'] = reviews_df['sentiment'].apply(lambda x: 'positive' if x > 0 else ('negative' if x < 0 else 'neutral'))
-print(reviews_df[['review_text', 'sentiment_label']].head())
-print(reviews_df['sentiment_label'].value_counts())
+sentiment_counts = reviews_df.groupby(['gender', 'sentiment_label']).size().unstack(fill_value=0)
+print(sentiment_counts)
